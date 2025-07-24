a96aea1b63583ebcd174df2a28822488c15377ca
Carlos Villalobos <305770889@covao.ed.cr>
2025-07-24 14:01:30 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PitStop_Parts_Inventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PitStop-Parts-Inventario
+
