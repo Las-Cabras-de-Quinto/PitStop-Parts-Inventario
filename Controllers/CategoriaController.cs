98eb7618f9bda66f627a01a9d3b7151035866f9b
Luci <luciaorozcoleandro@gmail.com>
2025-07-28 20:17:37 -0600

    Categoria-Controller
    Sufrimiento

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -1,17 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PitStop_Parts_Inventario.Models;
+using PitStop_Parts_Inventario.Services;
+using PitStop_Parts_Inventario.Models.ViewModels;
 
 namespace PitStop_Parts_Inventario.Controllers
 {
     public class CategoriaController : BaseController
     {
-        private readonly ILogger<CategoriaController> _logger;
+       private readonly ILogger<CategoriaController> _logger;
+       private readonly CategoriaService _CategoriaService;
 
-        public CategoriaController(ILogger<CategoriaController> logger)
-        {
-            _logger = logger;
-        }
+       public CategoriaController(ILogger<CategoriaController> logger, CategoriaService categoriaService)
+       {
+                _logger = logger;
+                _CategoriaService = categoriaService;
+       }
+
+       public async Task<IActionResult> Index(int numeroPagina, CategoriaFilterOptions filtros)
+       {
+            // Usar los parámetros recibidos para consultar el servicio
+            var resultado = await _CategoriaService.GetPagedAsync(
+            numeroPagina,
+            10,
+            filtros
+            );
+            return View(resultado);
+            }
 
         public IActionResult Index()
         {
