9d992e409718aecb1064be060134b78abcef8a07
Fabrots <fjosuets08@gmail.com>
2025-07-28 19:55:49 -0600

    Controlador Producto
    Hice el controlador del Producto

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PitStop_Parts_Inventario.Models;
+using PitStop_Parts_Inventario.Services;
+using PitStop_Parts_Inventario.Models.ViewModels;
+using PitStop_Parts_Inventario.Services.Interfaces;
 
 namespace PitStop_Parts_Inventario.Controllers
 {
     public class ProductoController : BaseController
     {
         private readonly ILogger<ProductoController> _logger;
+        private readonly ProductoService _ProductoService;
 
-        public ProductoController(ILogger<ProductoController> logger)
+        public ProductoController(ILogger<ProductoController> logger, ProductoService productoService)
         {
             _logger = logger;
+            _ProductoService = productoService;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int numeroPagina, ProductoFilterOptions filtros)
         {
-            return View();
+            // Usar los parámetros recibidos para consultar el servicio
+            var resultado = await _ProductoService.GetPagedAsync(
+                numeroPagina,
+                10,
+                filtros
+            );
+            return View(resultado);
         }
 
         public IActionResult Privacy()
