95a60b843b904e8f7fc41762a15e6553b36e3c3f
Juan Carlos <305770811@covao.ed.cr>
2025-07-28 19:56:40 -0600

    Update Proveedor Controller
    Adición de using services , viewmodels e
    Index

--- Controllers/ProveedorController.cs
+++ Controllers/ProveedorController.cs
@@ -1,21 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PitStop_Parts_Inventario.Models;
+using PitStop_Parts_Inventario.Services;
+using PitStop_Parts_Inventario.Models.ViewModels;
+
+
 
 namespace PitStop_Parts_Inventario.Controllers
 {
     public class ProveedorController : BaseController
     {
         private readonly ILogger<ProveedorController> _logger;
+        private readonly ProveedorService _ProveedorService;
 
-        public ProveedorController(ILogger<ProveedorController> logger)
+        public ProveedorController(ILogger<ProveedorController> logger, ProveedorService proveedorService)
+
         {
             _logger = logger;
+            _ProveedorService = proveedorService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int numeroPagina, ProveedorFilterOptions filtros)
         {
-            return View();
+            // Usar los parámetros recibidos para consultar el servicio
+            var resultado = await _ProveedorService.GetPagedAsync(
+                numeroPagina,
+                10,
+                filtros
+            );
+            return View(resultado);
         }
 
         public IActionResult Privacy()
